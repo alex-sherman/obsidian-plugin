e52dbda0aaea04917dc643ed547938abb5c9ecf6
Alex Sherman <asherman1024@gmail.com>
2026-01-18 22:17:41 -0600

    Change to path, reduce lag, remove logs.

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,8 @@ export default class AlexShermanPlugin extends Plugin {
   async onload() {
     this.registerEvent(
       this.app.workspace.on("active-leaf-change", (leaf) => {
-        if (!leaf) return;
-        if (leaf.view instanceof FileView) {
-          leaf.view.containerEl.dataset.file = leaf.view.file?.path;
-          console.log(leaf.view.containerEl);
-          console.log(leaf.view.file);
-        } else {
-          leaf.view.containerEl.dataset.file = undefined;
+        if (leaf?.view instanceof FileView) {
+          leaf.view.contentEl.dataset.path = leaf.view.file?.path;
         }
       }),
     );
